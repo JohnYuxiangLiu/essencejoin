3f4e0f140ac0329371a7edc6f7e42c3b070202ac
John Liu <2192737@gmail.com>
2020-04-04 15:17:49 +1300

    activity.activityName in homepage working

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,14 +1,25 @@
+const activityModel = require("../models/activityModel");
 
+exports.getOverview = async (req, res) => {
+  try {
+    //1. find all activities from database collection
+    const activities=await activityModel.find()
 
-exports.getOverview=(req, res) => {
+    //2. build template
+
+    //3. render template
+
     res.status(200).render("overview", {
-      data: "overview data"
+      title: "All Activities",
+      activities,
     });
-  };
+  } catch (err) {
+    next(err)
+  }
+};
 
-exports.getActivity=(req, res) => {
-    res.status(200).render("activity", {
-      data: "activity data"
-    });
-  };
-
+exports.getActivity = (req, res) => {
+  res.status(200).render("activity", {
+    data: "activity data"
+  });
+};
